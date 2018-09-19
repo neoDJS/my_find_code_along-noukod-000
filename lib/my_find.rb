8cfa7d42d48fd9463d22e7be071cf89c5fbdeafa
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-09-19 20:38:31 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,8 +2,6 @@ require 'pry'
 
 def my_find(collection)
   collection.length.times do |i|
-    if yield(collection[i])
-      return collection[i]
-    end
+    return collection[i] if yield(collection[i])
   end
 end
